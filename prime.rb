740d57de39e40de24d20c33977988b90e051e192
Brett Weisberg <weisberg.brett@gmail.com>
2018-03-15 23:40:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(n)
   numbers = (-100..100).to_a
   if 0 < n
     numbers.any? do |integer|
-    n/1 == n  && n/integer == 1 &&  || n%integer != 0
+    n/1 == n  && n/integer == 1 || n%integer != 0
 
   end
  end
